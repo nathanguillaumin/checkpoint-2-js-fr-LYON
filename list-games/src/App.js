ce182ef1136ff84d12390e6ad02791926019ec7d
tmicka23 <mickathp42@gmail.com>
2020-05-07 09:50:16 +0200

    create list games app & remove cra content

---
+++ list-games/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <main className="App">
+      <h1>Hello Wild Checkpoint 2</h1>
+    </main>
+  );
+}
+
+export default App;
