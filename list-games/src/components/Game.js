07385bdb8595c669583068d1b44d2cf421fe599c
tmicka23 <mickathp42@gmail.com>
2020-05-07 11:01:29 +0200

    display & filter games & handle errors

--- list-games/src/components/Game.js
+++ list-games/src/components/Game.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Game = (props) => {
-  return <h2>Hello from Game !</h2>;
+  return (
+    <div>
+      <h3>{props.name}</h3>
+      <span>{props.rating} / 5</span>
+      <img src={props.background_image} alt={props.name} />
+    </div>
+  );
 };
 
 export default Game;
--- list-games/src/components/GameList.js
+++ list-games/src/components/GameList.js
@@ -7,16 +7,21 @@ class GameList extends React.Component {
     super(props);
     this.state = {
       list: null,
+      bestGames: false,
     };
   }
 
   getList = async () => {
     const list = await Axios.get(
-      "https://wild-games.herokuapp.com/api/v2"
+      "https://wild-games.herokuapp.com/api/v1"
     ).catch((e) => {
-      return `Nous avons rencontrer une erreur lors du chargement des jeux ! -> ${e}`;
+      return `Nous avons rencontrer une erreur lors du chargement des jeux ! -> \n ${e}`;
     });
     this.setState({ list: list.statusText === "OK" ? list.data : list });
+  };
+
+  handleBests = () => {
+    this.setState({ bestGames: !this.state.bestGames });
   };
 
   componentDidMount() {
@@ -24,11 +29,27 @@ class GameList extends React.Component {
   }
 
   render() {
-    console.log(this.state.list);
+    const { list, bestGames } = this.state;
+
     return (
       <>
-        <h2>Hello from Gamelist !</h2>
-        <Game />
+        <h2>See our Amazing Game List !</h2>
+        <button onClick={this.handleBests}>
+          {bestGames ? "All Games" : "Best Games"}
+        </button>
+        {typeof list === "string" ? (
+          <div>{list}</div>
+        ) : list !== null ? (
+          !bestGames ? (
+            list.map((game) => <Game {...game} key={game.id} />)
+          ) : (
+            list
+              .filter((item) => item.rating >= 4.5)
+              .map((game) => <Game {...game} key={game.id} />)
+          )
+        ) : (
+          <h2>Loading ...</h2>
+        )}
       </>
     );
   }
