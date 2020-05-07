aa2828c9bc22188fc5cbc0a34b072bdcaa0d45f4
K0Si-003 <hugo.pioda@gmail.com>
2020-05-07 09:52:50 +0200

    Init component and style

---
+++ list-games/src/components/Game.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Game extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      gameId: ''
+    };
+  }
+
+  render () {
+    return (
+      <div>Test</div>
+    );
+  }
+}
+
+export default Game;
