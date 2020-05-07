7095b569bae1ac53eb814dd14b12ac928bf9c6d8
tmicka23 <mickathp42@gmail.com>
2020-05-07 12:30:42 +0200

    re-up state from GameList to App

--- list-games/src/App.js
+++ list-games/src/App.js
@@ -1,14 +1,47 @@
 import React from "react";
 import "./App.css";
+import Axios from "axios";
 import GameList from "./components/GameList";
 
-function App() {
-  return (
-    <main className="App">
-      <h1>Hello Wild Checkpoint 2</h1>
-      <GameList />
-    </main>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: null,
+    };
+  }
+
+  getList = async () => {
+    const list = await Axios.get(
+      "https://wild-games.herokuapp.com/api/v1"
+    ).catch((e) => {
+      return `Nous avons rencontrer une erreur lors du chargement des jeux ! -> \n ${e}`;
+    });
+    this.setState({ list: list.statusText === "OK" ? list.data : list });
+  };
+
+  removeGame = (e) => {
+    let list = this.state.list.slice();
+    list = list.filter((item) => item.id !== parseInt(e.target.id));
+    this.setState({ list });
+  };
+
+  componentDidMount() {
+    this.getList();
+  }
+
+  render() {
+    return (
+      <main className="App">
+        <h1>Hello Wild Checkpoint 2</h1>
+        <GameList
+          getList={this.getList}
+          remove={this.removeGame}
+          {...this.state}
+        />
+      </main>
+    );
+  }
 }
 
 export default App;
--- list-games/src/components/GameList.js
+++ list-games/src/components/GameList.js
@@ -1,43 +1,22 @@
 import React from "react";
 import Game from "./Game";
-import Axios from "axios";
 import "./GameList.css";
 
 class GameList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: null,
       bestGames: false,
     };
   }
-
-  getList = async () => {
-    const list = await Axios.get(
-      "https://wild-games.herokuapp.com/api/v1"
-    ).catch((e) => {
-      return `Nous avons rencontrer une erreur lors du chargement des jeux ! -> \n ${e}`;
-    });
-    this.setState({ list: list.statusText === "OK" ? list.data : list });
-  };
 
   handleBests = () => {
     this.setState({ bestGames: !this.state.bestGames });
   };
 
-  removeGame = (e) => {
-    let list = this.state.list.slice();
-    list = list.filter((item) => item.id !== parseInt(e.target.id));
-    this.setState({ list });
-    console.log(list);
-  };
-
-  componentDidMount() {
-    this.getList();
-  }
-
   render() {
-    const { list, bestGames } = this.state;
+    const { bestGames } = this.state;
+    const { list } = this.props;
 
     return (
       <>
@@ -51,7 +30,7 @@ class GameList extends React.Component {
           ) : list !== null ? (
             !bestGames ? (
               list.map((game) => (
-                <Game {...game} key={game.id} remove={this.removeGame} />
+                <Game {...game} key={game.id} remove={this.props.remove} />
               ))
             ) : (
               list
