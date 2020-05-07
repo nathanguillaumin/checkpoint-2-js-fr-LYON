dac2c0c26968f2a4f4877769f84f422b29daa7ec
ikramb-ctrl <b_zakia@homail.com>
2020-05-07 12:25:36 +0200

    modification composants

--- list-games/src/components/Game.js
+++ list-games/src/components/Game.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import GameList from './GameList';
+
 
 class Game extends Component {
   constructor(props) {
@@ -8,8 +10,10 @@ class Game extends Component {
     this.state = {
       game: {
         id: 0,
-        name: ''
-    
+        name: '',
+        background_image:'',
+        rating: ''
+      
       }
     };
   }
@@ -32,16 +36,18 @@ class Game extends Component {
   }
 
   loadGame(gameId) {
-    const url = `https://wild-games.herokuapp.com/api/v1${gameId}`;
+    const url = `https://wild-games.herokuapp.com/api/v1/${gameId}`;
+    
     axios
       .get(url)
       .then(res => res.data)
       .then(data => {
         this.setState({
-          game: {
+          character: {
             id: data.id,
-            name: data.name
-            
+            name: data.name,
+            background_image: data.background_image,
+            rating: data.rating
           }
         });
       });
@@ -50,13 +56,37 @@ class Game extends Component {
   render() {
     const game = this.state.game;
     return (
-      <div className="Game">
-        <h1>List of Games </h1>
+      <div>
+        <header className="App-header">
+          <h1 className="App-title">List of Games</h1>
+          
+          <p>
+            <Link to="/">Back to Games </Link>
+          </p>
+        </header>
+        <div className="Game">
+        <img src={game.background_image} alt={game.name} />
 
+          <ul>
+            <li>
+              ID: <strong>{game.id}</strong>
+            </li>
+            <li>
+              Name: <strong>{game.name}</strong>
+            </li>
+            <li>
+              Rating: <strong>{game.rating}</strong>
+            </li>
+            <li>
+              background_image: <strong>{game.background_image}</strong>
+            </li>
+
+          </ul>
+        </div>
         <GameList />
-        
+
       </div>
-     
+
     );
   }
 }
--- list-games/src/components/GamesList.js
+++ list-games/src/components/GamesList.js
@@ -1,23 +1,22 @@
-import React from 'react';
+import React from "react";
 import { Link } from 'react-router-dom';
-import game from './game.json';
+import game from "../game.json";
 
-function GamesList() {
+function GameList() {
   return (
-    <div className="GamesList">
-      <h3>List of Games</h3>
-
+    <div className="GameList">
+     
       {game.map(game => (
         <p key={game.id} className="Game">
 
           <Link to={`/game/${game.id}`}>{game.name}</Link>
 
         </p>
+
       ))}
 
     </div>
   );
 }
 
-
-export default  GamesList;
+export default GameList;
