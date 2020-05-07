66b0e064aa9580d7a6f70180e1040fd69a646e6e
Amélie <ameliechevalier3006@gmail.com>
2020-05-07 10:27:28 +0200

    displaying games with title, rating, pictures

--- list-games/src/components/Game.js
+++ list-games/src/components/Game.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import './Game.css';
 
 const Game = ({ gameList }) => {
 
   console.log(gameList)
-    return (
-      
-    <p>{gameList[0].name}</p>
-    )
-  }
+
+  return (
+    <>
+      <p>Voici la liste des jeux disponibles</p>
+      <div className='game-container'>
+        {gameList.map(game => {
+          return (
+            <div className='game'>
+              <div className='game-presentation'>
+                <h1>{game.name}</h1>
+                <p>Note : {game.rating}</p>
+              </div>
+              <img src={game.background_image} />
+            </div>
+          )
+        })}
+      </div>
+    </>
+  )
+}
 
 export default Game;
