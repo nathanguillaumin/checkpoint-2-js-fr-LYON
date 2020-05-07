39c99e8c9ac2b7493115477747b01bee6988a8fa
tmicka23 <mickathp42@gmail.com>
2020-05-07 10:16:26 +0200

    install axios & get data from API & catch errors

--- list-games/src/components/GameList.js
+++ list-games/src/components/GameList.js
@@ -1,12 +1,30 @@
 import React from "react";
 import Game from "./Game";
+import Axios from "axios";
 
 class GameList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      list: null,
+    };
   }
+
+  getList = async () => {
+    const list = await Axios.get(
+      "https://wild-games.herokuapp.com/api/v2"
+    ).catch((e) => {
+      return `Nous avons rencontrer une erreur lors du chargement des jeux ! -> ${e}`;
+    });
+    this.setState({ list: list.statusText === "OK" ? list.data : list });
+  };
+
+  componentDidMount() {
+    this.getList();
+  }
+
   render() {
+    console.log(this.state.list);
     return (
       <>
         <h2>Hello from Gamelist !</h2>
