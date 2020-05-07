7d6867da7593e340b6ade8b30d522c12506e9cdd
Amélie <ameliechevalier3006@gmail.com>
2020-05-07 13:16:29 +0200

    GOT SCREENSHOTS

--- list-games/src/components/ScreenShots.js
+++ list-games/src/components/ScreenShots.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import './ScreenShots.css';
 
 function ScreenShots (props) {
 
   const params = props.match.params
   console.log(params.id)
   const currentGame = props.gameList.filter(game => game.id === parseInt(params.id))
-  console.log(currentGame)
+  console.log(currentGame[0].short_screenshots)
   
   return (
   <div className='screen-page'>
     <Link to="/">Retourner à la liste des jeux</Link>
     <p>Voici les screenshots disponibles pour le jeu {currentGame[0].name}</p>
     <div className='screen-container'>
-      
-      
+      {currentGame[0].short_screenshots.map(screen => <li> <img src={screen.image} /></li>)}
     </div>
   </div>
   )}
