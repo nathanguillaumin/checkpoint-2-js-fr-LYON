3a3d2fa47124741fd138d346e805d1e933c02c9d
Geoffroy <geoffroy.crabieres@gmail.com>
2020-05-07 14:20:11 +0200

    added back to homepage button + CSS

--- list-games/src/components/GameScreenshots.js
+++ list-games/src/components/GameScreenshots.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import '../styles/gameScreenshots.css';
 import { useParams } from 'react-router-dom';
 
 function GameScreenshots(props) {
 
     const params = props.match.params;
     return (
-        <div className='screenshot-container'>
-            {props.allGames.filter(singleGame => singleGame.slug === params.GameSlug).map(singleGame => (
-                singleGame.short_screenshots.map(singleScreenShot => (
-                <img className='media' src={singleScreenShot.image}/>
-            ))))}
-        </div>
+        <>
+        <a href='/'><p className='p-with-link'>Back to Homepage</p></a>
+            <div className='screenshot-container'>
+                {props.allGames.filter(singleGame => singleGame.slug === params.GameSlug).map(singleGame => (
+                    singleGame.short_screenshots.map(singleScreenShot => (
+                    <img className='media' src={singleScreenShot.image}/>
+                ))))}
+            </div>
+        </>
     )
 }
 
