e140f77c87c996fa6b0d08c7b5a8ae199ddbf64e
Riri Z <riri.zamor@gmail.com>
2020-05-07 10:31:46 +0200

    npm i  react router

--- list-games/src/App.js
+++ list-games/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Gamelist from './Components/Gamelist';
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 function App() {
   return (
