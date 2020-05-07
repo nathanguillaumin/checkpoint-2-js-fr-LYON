a8766762c881433f1421608634770e1ad37a4794
Amélie <ameliechevalier3006@gmail.com>
2020-05-07 11:22:44 +0200

    create function to remove game from the list

--- list-games/src/components/Game.js
+++ list-games/src/components/Game.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Game.css';
 
 const Game = ({ gameList }) => {
 
+  const [customGameList, setCustomGameList] = useState(gameList);
+
   return (
-    
     <>
       <p>Voici la liste des jeux disponibles</p>
       <div className='game-container'>
-        {gameList.map(game => {
+        {customGameList.map(game => {
           return (
             <div className='game' key={game.name}>
               <div className='game-presentation'>
                 <h1>{game.name}</h1>
                 <p>Note : {game.rating}</p>
-                <button
-                  onClick={() => console.log(game.name)}
+                <button 
+                  onClick={() => {
+                    const id = customGameList.indexOf(game)
+                    setCustomGameList(customGameList.filter( (game, index) =>  index !== id))
+                  }}
                 >Supprimer ce jeu de la liste</button>
               </div>
               <img src={game.background_image} alt={game.name}/>
