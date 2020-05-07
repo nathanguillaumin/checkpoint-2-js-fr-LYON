4ad532d17f3d5c468874516aecc6bb17bea76cbe
Riri Z <riri.zamor@gmail.com>
2020-05-07 10:37:00 +0200

    create route & switch

--- list-games/src/App.js
+++ list-games/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import Gamelist from './Components/Gamelist';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import Screenshots from './Components/Screenshots';
 
 function App() {
   return (
     <div className="App">
      <Gamelist/>
+     <Switch>
+       <Route exact path = "/" component={Gamelist}></Route>
+       <Route exact path ="/jeu/screenshots/:id" component={Screenshots}></Route>
+     </Switch>
     </div>
   );
 }
