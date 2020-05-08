cff5067986c459751cb466ee32b3224d8fa782cf
Anaïs Huoch <anaisgagneur@gmail.com>
2020-05-08 20:55:44 +0200

    button filter partially working

--- list-games/src/App.js
+++ list-games/src/App.js
@@ -6,7 +6,9 @@ export default class App extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      games: []
+      games: [],
+      filterGames : false,
+      buttonText: 'Best games',
     };
   }
 
@@ -28,11 +30,36 @@ export default class App extends Component {
     this.setState({games: [...this.state.games.filter(game => game.name !== name)]});
   }
 
+  bestGamesOnClick = () => {
+    let games = this.state.games.slice();
+    if (this.state.filterGames === false) {
+      games = games.filter(
+        game => game.rating >= 4.5
+      );
+      this.setState({ games });
+      this.setState({ filterGames: true });
+      this.setState({ buttonText: 'Best games' });
+    } else {
+      this.setState({ quoteList: this.quoteListRef });
+      this.setState({ filterGames: false });
+      this.setState({ buttonText: 'All games' });
+    }
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <GameList games={this.state.games} delGame={this.delGame}/>
+      <div>
+        <button onClick={this.bestGamesOnClick}>
+            {this.state.buttonText}
+        </button>
+        {this.state.games.map((game, index) => (
+          <div key={index}>
+            <GameList games={this.state.games} delGame={this.delGame}/>
+          </div>
+        ))}
       </div>
     );
   }
 }
+
