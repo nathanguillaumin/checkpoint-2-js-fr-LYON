190a94bd1f069b0775a2c6a45c5be69675585658
Rincevent42 <porvooc@gmail.com>
2020-05-08 17:11:59 +0200

    Screenshots page displays images

--- list-game/src/components/GameList.js
+++ list-game/src/components/GameList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Game from './Game';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Screenshots from './Screenshots';
 
 class GameList extends React.Component {
@@ -10,23 +10,46 @@ class GameList extends React.Component {
     this.state = {
       allGameList: [],
       highRateGames: [],
-      allGames: true
+      allGames: true,
+      loading: true
     };
   }
 
-  getData = () => {
-    const url = 'https://wild-games.herokuapp.com/api/v1'
-    axios
-      .get(url)
-      .then(response => {
-        const allGameList = response.data;
-        this.setState({ allGameList });
-      })
-      .catch(error => {
-        console.log(error);
-        alert('Une erreur s\'est produite lors du chargement des données.');
-      });
-  }
+  getData = async () => {
+    try {
+      let response = await axios.get('https://wild-games.herokuapp.com/api/v1');
+      const allGameList = response.data;
+      this.setState({ allGameList, loading: false });
+    } catch (error) {
+          // Error 😨
+        if (error.response) {
+          /*
+          * The request was made and the server responded with a
+          * status code that falls out of the range of 2xx
+          */
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+      } else if (error.request) {
+          /*
+          * The request was made but no response was received, `error.request`
+          * is an instance of XMLHttpRequest in the browser and an instance
+          * of http.ClientRequest in Node.js
+          */
+          console.log(error.request);
+      } else {
+          // Something happened in setting up the request and triggered an Error
+          console.log('Error', error.message);
+      }
+      console.log(error);
+    }
+  };
+
+  getData2 = async () => {
+    let res = await axios.get('https://wild-games.herokuapp.com/api/v1');
+    const allGameList = res.data;
+    this.setState({ allGameList });
+  };
 
   componentDidMount () {
     this.getData();
@@ -40,30 +63,31 @@ class GameList extends React.Component {
   }
 
   handleRatedGames () {
-    const highRateGames = this.state.allGameList.filter(game => game.rating >= 4.5);
+    const highRateGames = this.state.allGameList.filter((game) => game.rating >= 4.5);
     this.setState({ highRateGames, allGames: !this.state.allGames });
   }
 
    render () {
      const userList = (this.state.allGames ? this.state.allGameList : this.state.highRateGames)
        const list = userList.map(game =>
-      <>
-        <Game game={game} />
-        {this.state.allGames && <button key={`${game.id}`} onClick={() => this.handleDeleteGame(game.id)}>Delete {game.name}</button>}
-      </>
+        <Game game={game} onClick={this.handleDeleteGame} />
       );
      
     return (
-      <Router>
-        <div>
-          <h1>Welcome to the game list of a non-gamer guy!</h1>
-          <button onClick={() => this.handleRatedGames()}>{this.state.allGames ? 'Best games' : 'All games'}</button>
-          {list}
-        </div>
-        <Switch>
-          <Route path='/:jeu/screenshots/:id' render={(props) => (<Screenshots {...this.state} {...props} />)} />
-        </Switch>
-      </Router>
+      <div>
+        { (this.state.loading) ? (<p>Loading data...</p>) : (
+        <Router>
+          <div>
+            <h1>Welcome to the game list of a non-gamer guy!</h1>
+            <button onClick={() => this.handleRatedGames()}>{this.state.allGames ? 'Best games' : 'All games'}</button>
+            {list}
+          </div>
+          <Switch>
+            <Route path='/:jeu/screenshots/:id' render={(props) => (<Screenshots {...this.state} {...props} />)} />
+          </Switch>
+        </Router>
+        )}
+      </div>
     );
   }
 }
--- list-game/src/components/Screenshots.js
+++ list-game/src/components/Screenshots.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Screenshots.css';
 
 const Screenshots = (props) => {
   const params = props.match.params;
+  const game = props.allGameList.find((game) => game.id === parseInt(params.id));
+  /* console.log(game); */
+  const screenshotsArray = game.short_screenshots;
+  /* console.log(screenshotsArray); */
+  const imagesList = screenshotsArray.map(image => <img key={image.id} src={image.image} alt={game.name} />);
   return (
-    <div>
-      <p>Game Id: {params.id}</p>
+    <div className='screenshots'>
+      <h2>Screenshots of the game: {game.name}</h2>
+      {imagesList}
+      <p><Link to='/'>Back home</Link></p>
     </div>
   );
-}
+};
 
 export default Screenshots;
