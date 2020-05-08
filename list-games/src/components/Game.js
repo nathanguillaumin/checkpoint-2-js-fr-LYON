21eefa8887859666fc359c00c3df2051eb5fb826
Anaïs Huoch <anaisgagneur@gmail.com>
2020-05-08 14:13:26 +0200

    indent fixing

--- list-games/src/components/Game.js
+++ list-games/src/components/Game.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 export default function Game(props) {
-return (
-<div>
-<h1>Name:{props.name}</h1>
-<p>released : {props.released}</p> 
-<p>rating : {props.rating}</p> 
-<img src={props.background_image} alt={props.name} /> 
-</div>
-)
+  return (
+    <div>
+      <h1>Name:{props.name}</h1>
+      <p>released : {props.released}</p> 
+      <p>rating : {props.rating}</p> 
+      <img src={props.background_image} alt={props.name} /> 
+  </div>
+  )
 }
